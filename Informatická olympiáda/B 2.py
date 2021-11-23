63416b311f3e39a8fac4b6577e84c6c9e93a5044
AllCreativeNamesAreUsed <76529048+AllCreativeNamesAreUsed@users.noreply.github.com>
2021-11-23 20:23:33 +0100

    Update B 2.py

--- Informatická olympiáda/B 2.py
+++ Informatická olympiáda/B 2.py
@@ -22,4 +22,25 @@ for i in range(a, b):
 print(amount_of_zeros)
 
 
+number = 1
+amount_of_zeros = 0
+a = int(input())
+b = int(input())+1
 
+if a == 0:
+    a = 1
+
+for f in range(1, a):
+    number *= f
+
+for i in range(a, b):
+    number *= i
+
+    for l in range(len(str(number))):
+        if number % 10 == 0:
+            amount_of_zeros += b-i
+            number = number // 10
+        else:
+            number = int(str(number)[-1])
+            continue
+print(amount_of_zeros)
