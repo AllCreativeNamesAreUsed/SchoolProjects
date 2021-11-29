277ef55fd5a87757e68c8b2435028474abba2939
AllCreativeNamesAreUsed <76529048+AllCreativeNamesAreUsed@users.noreply.github.com>
2021-11-29 22:39:16 +0100

    Delete Informatická olympiáda directory

--- Informatická olympiáda/B 1.py
+++
@@ -1,8 +0,0 @@
-n = input()
-board = list(map(int, input().split()))
-ledder_lengths = sorted(list(set(board) - {-1,0}) ,reverse=True)
-list_of_ledders = []
-for i in ledder_lengths:
-    start = board.index(i)
-    list_of_ledders.append([start + 1, i, start + i +1])
-print(list_of_ledders)
